eb90a062ca6495aa0ed11d6eea9b95f42632edb7
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-04-06 13:59:07 +0200

    directives reviewed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'omer omeroglu';
   imgURL = "https://picsum.photos/200"
+  images = [
+    "https://picsum.photos/200",
+    "https://picsum.photos/201",
+    "https://picsum.photos/202"
+  ]
   currentDate = new Date()
   cost = 2000
   temprature = 25.3
+  blueClass = false
+  fontSize = 16
 
 
   getName() {
